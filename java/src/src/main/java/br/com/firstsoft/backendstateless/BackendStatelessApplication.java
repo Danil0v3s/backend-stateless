1f70630b06313888985339654f1dd79e9bf60f1b
Danilo Lemes <daniloleemes@hotmail.com>
2018-08-05 17:10:08 -0300

    Fix docker

--- java/src/src/main/java/br/com/firstsoft/backendstateless/BackendStatelessApplication.java
+++ java/src/src/main/java/br/com/firstsoft/backendstateless/BackendStatelessApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BackendStatelessApplication {
-
 	public static void main(String[] args) {
+		System.out.println("Teste");
 		SpringApplication.run(BackendStatelessApplication.class, args);
 	}
 }
