7496c8fe034025a40b2336a8f0bdb9b3bcf7165e
Danilo Lemes <daniloleemes@hotmail.com>
2018-08-09 00:10:49 -0300

    CHECAR SE JA EXISTE ESSE SCAN REQUEST. SE COM STATUS DE ERRO = RETENTAR. SE PROCESSANDO/SUCESSO = IGNORAR.

--- java/src/src/main/java/br/com/firstsoft/backendstateless/services/InvoiceService.java
+++ java/src/src/main/java/br/com/firstsoft/backendstateless/services/InvoiceService.java
@@ -1,10 +1,14 @@
 package br.com.firstsoft.backendstateless.services;
 
 import br.com.firstsoft.backendstateless.business.dto.NotaFiscalDTO;
+import br.com.firstsoft.backendstateless.business.enums.ScanRequestResult;
 import br.com.firstsoft.backendstateless.business.vo.ScanRequest;
+import br.com.firstsoft.backendstateless.business.vo.User;
+import br.com.firstsoft.backendstateless.repository.ScanRequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -13,6 +17,8 @@ import java.beans.IntrospectionException;
 import java.beans.PropertyDescriptor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Date;
+import java.util.Optional;
 
 @Service
 public class InvoiceService {
@@ -21,16 +27,68 @@ public class InvoiceService {
 //    private InvoiceRepository invoiceRepository;
 
     @Autowired
+    private ScanRequestRepository scanRequestRepository;
+
+    @Autowired
     private Environment environment;
 
-    public void requestInvoiceScan(ScanRequest scanRequest) {
+    public ResponseEntity<ScanRequest> requestInvoiceScan(ScanRequest scanRequest) {
 
+        Optional<ScanRequest> optionalScanRequest = scanRequestRepository.findById(scanRequest.getCHashQRCode());
+        ScanRequest managedScanRequest;
+
+        if (optionalScanRequest.isPresent()) {
+            managedScanRequest = optionalScanRequest.get();
+            if (managedScanRequest.getScanRequestResult() == ScanRequestResult.ERROR) {
+                generateInvoiceScan(managedScanRequest);
+            }
+        } else {
+            managedScanRequest = saveScanRequest(scanRequest);
+            generateInvoiceScan(managedScanRequest);
+        }
+
+        return ResponseEntity.ok(managedScanRequest);
+    }
+
+    private void generateInvoiceScan(ScanRequest managedScanRequest) {
         RestTemplate restTemplate = new RestTemplate();
 
-        UriComponentsBuilder uriBuilder = UriComponentsBuilder
-                .fromHttpUrl(environment.getProperty("backend-stateless.scan-service.url", ""));
+        UriComponentsBuilder uriBuilder = getUriBuilder();
+        generateQueryParams(managedScanRequest, uriBuilder);
 
-        /**
+        Thread scanThread = new Thread(() -> {
+            ResponseEntity responseEntity = restTemplate.getForEntity(uriBuilder.toUriString(), NotaFiscalDTO.class);
+            NotaFiscalDTO notaFiscalDTO = (NotaFiscalDTO) responseEntity.getBody();
+
+            if (notaFiscalDTO != null) {
+                managedScanRequest.setScanRequestResult(ScanRequestResult.SUCCESS);
+            } else {
+                managedScanRequest.setScanRequestResult(ScanRequestResult.ERROR);
+            }
+
+            /*
+             * TODO: GERAR A NOTA FISCAL QUE SERA SALVA NO BANCO, NORMALIZADA. DEFINIR ESTRUTURA DA ENTIDADE.
+             */
+            scanRequestRepository.save(managedScanRequest);
+        });
+        scanThread.start();
+    }
+
+    private ScanRequest saveScanRequest(ScanRequest scanRequest) {
+        User loggedUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        scanRequest.setUser(loggedUser);
+        scanRequest.setRequestedAt(new Date());
+        scanRequest.setScanRequestResult(ScanRequestResult.PROCESSING);
+        return scanRequestRepository.save(scanRequest);
+    }
+
+    private UriComponentsBuilder getUriBuilder() {
+        return UriComponentsBuilder
+                .fromHttpUrl(environment.getProperty("backend-stateless.scan-service.url", ""));
+    }
+
+    private void generateQueryParams(ScanRequest scanRequest, UriComponentsBuilder uriBuilder) {
+        /*
          * Reflexão
          * Seria melhor usar o método que cacheia
          * https://stackoverflow.com/questions/2638590/best-way-of-invoking-getter-by-reflection
@@ -44,19 +102,6 @@ public class InvoiceService {
                 e.printStackTrace();
             }
         }
-
-        /**
-         * TODO: RODAR EM THREAD, E VOLTAR UMA RESPOSTA DE OK PRO CLIENTE.
-         * TODO: GUARDAR O SCANREQUEST NO BANCO VINCULADO AO USUARIO.
-         * TODO: QUANDO A RESPOSTA DO PYTHON VOLTAR, MARCAR O STATUS DO SCANREQUEST DE ACORDO COM A RESPOSTA DO PYTHON, SUCESSO/ERRO, ETC
-         */
-        ResponseEntity responseEntity = restTemplate.getForEntity(uriBuilder.toUriString(), NotaFiscalDTO.class);
-        NotaFiscalDTO notaFiscal = (NotaFiscalDTO) responseEntity.getBody();
-
-        if (notaFiscal != null) {
-//            invoiceRepository.save(notaFiscal);
-        }
-
     }
 
 }
